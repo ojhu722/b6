7e7e4631fa60add8cf3e81174a37d57407b1dc28
Yashx3184 <yashwanthgowdar2005@gmail.com>
2024-09-23 14:21:45 +0000

    first

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/ddos/vikku.py",
+    "postStartCommand": "python3 /workspaces/bgmi_ddos/vikku.py",
     "customizations": {
         "vscode": {
             "settings": {
